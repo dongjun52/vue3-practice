c4d8dba5b7c9533f647668551ae34cc702b5b039
Andy <ldj5471@gmail.com>
2021-07-01 00:04:29 +0900

    Composition AIP first commit

---
+++ src/components/CompositionAPI.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <p ref="user">my name :</p>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+
+export default {
+  name: "CompositionAPI",
+
+  setup() {
+    const user = ref < String > "foo";
+    user.value = "andy";
+
+    onMounted(() => {
+      console.log(user.value);
+    });
+    return {
+      user,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
